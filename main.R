0819c960d1bebe07f7be14ae63cf5831629ee9b5
Jezzaay <Jezzaay@users.noreply.github.com>
2020-02-03 20:52:20 +0000

    adjusting theme and working with image

--- main.R
+++ main.R
@@ -2,20 +2,24 @@
 
 library(tidyverse)
 library(cowplot)
+library(ggthemes)
 library(lubridate)
 df <- read_csv("AB_NYC_2019.csv")
 head(df)
-NYC <- draw_image("New_York_City.png")
-
-ggdraw() + NYC
-draw_image(NYC, x = 1, y =1)
+NYC <- draw_image("NYC.png", scale = 0.5)
 #plot(df$latitude,df$longitude, col="red", cex = .5)
 
+theme_set(theme_gdocs())
 
 df_man <- df %>% filter(neighbourhood_group == "Manhattan")
 head(df_man)
 
 
-manh<- ggplot(df, aes(x= latitude, y = longitude)) + geom_point() 
+manh<- ggplot(df, aes(x= latitude, y = longitude ,
+              colour = neighbourhood_group)) + 
+  geom_point() 
 
-manh 
+manh
+
+ggdraw(manh) +  draw_image("NYC.png", scale = 1)
+ 
