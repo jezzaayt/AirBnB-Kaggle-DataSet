18e02b8f3eb15cfebde88f6b5807990c6308f206
Jezzaay <Jezzaay@users.noreply.github.com>
2020-02-05 13:59:31 +0000

    Plots for different neighbourhoods

--- main.R
+++ main.R
@@ -9,17 +9,46 @@ head(df)
 NYC <- draw_image("NYC.png", scale = 0.5)
 #plot(df$latitude,df$longitude, col="red", cex = .5)
 
-theme_set(theme_gdocs())
+theme_set(theme_minimal())
 
 df_man <- df %>% filter(neighbourhood_group == "Manhattan")
+df_brook <- df %>% filter(neighbourhood_group == "Brooklyn")
+df_bronx <- df %>% filter(neighbourhood_group == "Bronx")
+df_queens <- df %>% filter(neighbourhood_group == "Queens")
+df_state_island <- df %>% filter(neighbourhood_group == "Staten Island")
 head(df_man)
 
+#ggdraw(manh) +  draw_image("NYC.png", scale = 1) attempting to get image on background
 
-manh<- ggplot(df, aes(x= latitude, y = longitude ,
+nyc_plot<- ggplot(df, aes(x= latitude, y = longitude ,
               colour = neighbourhood_group)) + 
-  geom_point() 
+  geom_point()
+nyc_plot  
 
-manh
+manh_plot <- ggplot(df_man, aes(x=latitude, y = longitude, 
+                                colour = neighbourhood)) +
+  geom_point()
 
-ggdraw(manh) +  draw_image("NYC.png", scale = 1)
- 
+manh_plot
+
+
+brook_plot <- ggplot(df_brook, aes(x=latitude, y=longitude,
+                                   colour=neighbourhood))+
+  geom_point()
+brook_plot
+
+bronx_plot <- ggplot(df_bronx, aes(x=latitude, y=longitude,
+                                   colour = neighbourhood)) +
+  geom_point()
+bronx_plot 
+
+queens_plot <- ggplot(df_queens, aes(x=latitude, y=longitude,
+                                     colour=neighbourhood)) + 
+  geom_point()
+queens_plot
+
+staten_plot <- ggplot(df_state_island,
+                      aes(x=latitude,  
+                          y=longitude,  colour=neighbourhood)) +
+  geom_point()
+staten_plot
