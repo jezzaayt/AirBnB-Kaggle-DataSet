161a5459a03c285d922db7e429ba68ee0cb80fc0
Jezzaay <Jezzaay@users.noreply.github.com>
2020-02-05 14:29:45 +0000

    price and review comparison scatter plot

--- main.R
+++ main.R
@@ -52,3 +52,25 @@ staten_plot <- ggplot(df_state_island,
                           y=longitude,  colour=neighbourhood)) +
   geom_point()
 staten_plot
+
+
+# Compare the number of reviews vs price
+# In the hope to see if more expensive AirBnB would have more reviews. 
+nyc_price_200 <- df %>% filter(price < 200 ) %>% filter(number_of_reviews >0)
+#limiting to 200 dollars a night though this is not counting min nights to stay
+# making sure there is at least one review
+nyc_price_plot <- ggplot(nyc_price_200, aes(x=number_of_reviews, y= price))+
+  geom_point() + coord_cartesian(xlim = c(0,700),
+                                                ylim= c(0,200))
+nyc_price_plot
+
+# From this plot we can see that places around 50 dollars a night get more reviews
+# But there are at least 200 - 400 reviews for anything over 50 dollars
+
+nyc_price_plot_co <- ggplot(nyc_price_200, aes(x=number_of_reviews, y= price, colour=neighbourhood_group))+
+  geom_point() + coord_cartesian(xlim = c(0,700),
+                                 ylim= c(0,200))
+nyc_price_plot_co
+
+# Now we can see what grouping they belong to
+# We can see more reviews are from Queens and around 3 from Manhattan have 600 ish reviews
